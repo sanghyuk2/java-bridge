2ee938b4dce1da43127f782709db0abba12a1ad5
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-11 12:06:28 +0900

    feat: 플레이어가 선택하는 key enum 구현

---
+++ src/main/java/bridge/domain/enums/Key.java
@@ -0,0 +1,16 @@
+package bridge.domain.enums;
+
+public enum Key {
+    UP("U"),
+    DOWN("D"),
+    RETRY("R"),
+    QUIT("Q");
+
+    private final String key;
+
+    Key(String key) {
+        this.key = key;
+    }
+
+
+}

53333e50dde2d5a63af878511b6a67491a39fb1b
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-11 12:06:52 +0900

    feat: 플레이어가 입력한 다리 사이즈만큼 다리 길이 생성 기능 구현

--- src/main/java/bridge/domain/BridgeGameManager.java
+++ src/main/java/bridge/domain/BridgeGameManager.java
@@ -1,18 +1,24 @@
 package bridge.domain;
 
+import bridge.BridgeMaker;
+import bridge.BridgeRandomNumberGenerator;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
 public class BridgeGameManager {
     private final InputView inputView;
     private final OutputView outputView;
+    private final BridgeMaker bridgeMaker;
+
 
     public BridgeGameManager() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
+        this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
     }
 
     public void start() {
-        inputView.readBridgeSize();
+        int bridgeSize = inputView.readBridgeSize();
+        bridgeMaker.makeBridge(bridgeSize);
     }
 }

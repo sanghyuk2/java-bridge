f280f410b018f7e035f966b9086cb24d081cab60
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-12-11 12:37:52 +0900

    feat: 움직임 실행하는 틀 구현

--- src/main/java/bridge/domain/BridgeGame.java
+++ src/main/java/bridge/domain/BridgeGame.java
@@ -9,8 +9,9 @@ public class BridgeGame {
      * 사용자가 칸을 이동할 때 사용하는 메서드
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * @param
      */
-    public void move() {
+    public void move(String movement) {
     }
 
     /**
@@ -19,5 +20,17 @@ public class BridgeGame {
      * 재시작을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void retry() {
+    }
+
+    public boolean isFail() {
+        return true;
+    }
+
+    public GameStatus gameStatus() {
+        return new GameStatus();
+    }
+
+    public boolean canMove() {
+        return true;
     }
 }
--- src/main/java/bridge/domain/BridgeGameManager.java
+++ src/main/java/bridge/domain/BridgeGameManager.java
@@ -11,17 +11,27 @@ public class BridgeGameManager {
     private final InputView inputView;
     private final OutputView outputView;
     private final BridgeMaker bridgeMaker;
+    private final BridgeGame bridgeGame;
 
 
     public BridgeGameManager() {
         this.inputView = new InputView();
         this.outputView = new OutputView();
         this.bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
+        this.bridgeGame = new BridgeGame();
     }
 
     public void start() {
         int bridgeSize = inputView.readBridgeSize();
         List<String> bridge = bridgeMaker.makeBridge(bridgeSize);
+        move();
+    }
 
+    private boolean move() {
+        while (bridgeGame.canMove()) {
+            bridgeGame.move(inputView.readMoving());
+            outputView.printMap(bridgeGame.gameStatus());
+        }
+        return bridgeGame.isFail();
     }
 }
